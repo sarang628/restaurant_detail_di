a6feba8ef033f3c21caeb17ffef0adf32eb42007
sryang <sarang628@gmail.com>
2025-06-25 15:21:05 +0800

    update

---
+++ RestaurantServiceModule.kt
@@ -0,0 +1,54 @@
+package com.sarang.torang.di.restaurant_detail_di
+
+import com.sarang.library.data.Feed
+import com.sarang.library.usecase.FetchReviewsUseCase
+import com.sarang.library.usecase.GetMenuUseCase
+import com.sarang.library.usecase.GetRestaurantGalleryUseCase
+import com.sarang.library.data.MenuData
+import com.sarang.library.data.RestaurantImage
+import com.sarang.torang.api.ApiRestaurant
+import com.sarang.torang.api.ApiReview
+import com.sarang.torang.api.handle
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.HttpException
+
+@InstallIn(SingletonComponent::class)
+@Module
+class RestaurantServiceModule {
+    @Provides
+    fun providesFetchReviewsUseCase(apiReview: ApiReview): FetchReviewsUseCase {
+        return object : FetchReviewsUseCase {
+            override suspend fun invoke(restaurantId: Int): List<Feed> {
+                try {
+                    return apiReview.getReviews(restaurantId).map { it.toFeedData() }
+                } catch (e: HttpException) {
+                    throw Exception(e.handle())
+                }
+            }
+        }
+    }
+
+
+    @Provides
+    fun providesGetRestaurantGalleryUseCase(
+        apiRestaurant: ApiRestaurant,
+    ): GetRestaurantGalleryUseCase {
+        return object : GetRestaurantGalleryUseCase {
+            override suspend fun invoke(restaurantId: Int): List<RestaurantImage> {
+                return apiRestaurant.getRestaurantDetail(restaurantId).toRestaurantImages()
+            }
+        }
+    }
+
+    @Provides
+    fun providesGetMenuUseCase(apiRestaurant: ApiRestaurant): GetMenuUseCase {
+        return object : GetMenuUseCase {
+            override suspend fun invoke(restaurantId: Int): List<MenuData> {
+                return apiRestaurant.getRestaurantDetail(restaurantId).toMenus()
+            }
+        }
+    }
+}
